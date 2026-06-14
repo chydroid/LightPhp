287847cb0b33dc883d8b8663ab8234468ba4d724
chydroid <chydroid@163.com>
2026-06-14 18:15:20 +0800

    feat: add favicon to homepage

--- app/view/templates/index.php
+++ app/view/templates/index.php
@@ -2,11 +2,12 @@
 <html>
 <head>
     <title><?php echo htmlspecialchars($title ?? 'Welcome'); ?></title>
+    <link rel="icon" href="/favicon.ico">
 </head>
 <body style="text-align:center;margin-top:80px">
     <img src="/assets/banner.png" alt="LightPHP" width="140">
     <h1>Welcome to LightPHP</h1>
     <p>A lightweight PHP framework</p>
-    <p style="margin-top:40px"><a href="https://github.com/chydroid/LightPhp" target="_blank">GitHub</a></p>
+    <p style="margin-top:40px">请在<a href="https://github.com/chydroid/LightPhp" target="_blank">GitHub</a>给本项目上颗小星星吧</p>
 </body>
 </html>

a552295cb848fe1da69c3bcb2fec58a3d84d1308
chydroid <chydroid@163.com>
2026-06-14 17:23:54 +0800

    feat: add GitHub link to homepage

--- app/view/templates/index.php
+++ app/view/templates/index.php
@@ -7,5 +7,6 @@
     <img src="/assets/banner.png" alt="LightPHP" width="140">
     <h1>Welcome to LightPHP</h1>
     <p>A lightweight PHP framework</p>
+    <p style="margin-top:40px"><a href="https://github.com/chydroid/LightPhp" target="_blank">GitHub</a></p>
 </body>
 </html>

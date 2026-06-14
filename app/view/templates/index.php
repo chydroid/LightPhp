5d7b74d8117ce0fe674c40b3a8d28402ad61aad0
chydroid <chydroid@163.com>
2026-06-14 17:11:49 +0800

    feat: use banner image and center homepage content

--- app/view/templates/index.php
+++ app/view/templates/index.php
@@ -3,8 +3,8 @@
 <head>
     <title><?php echo htmlspecialchars($title ?? 'Welcome'); ?></title>
 </head>
-<body>
-    <img src="/assets/logo.png" alt="LightPHP Logo" width="140">
+<body style="text-align:center;margin-top:80px">
+    <img src="/assets/banner.png" alt="LightPHP" width="140">
     <h1>Welcome to LightPHP</h1>
     <p>A lightweight PHP framework</p>
 </body>
